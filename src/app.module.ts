c09ac4146e9ccec28679ce554de6978532d18131
NullableDev <hopetoby@gmail.com>
2023-06-22 12:07:12 +0200

    feat: added request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { HealthModule } from "./modules/health/health.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { VerificationCodeModule } from "./utils/verification-code/verification-code.module";
@@ -6,6 +6,7 @@ import { MailerModule } from "./utils/mailer/mailer.module";
 import { ConfigModule } from "@nestjs/config";
 import { APP_FILTER } from "@nestjs/core";
 import { AllExceptionsFilter } from "./filters/allExceptions.filter";
+import RequestLoggerMiddleware from "./middleware/RequestLogger";
 
 @Module({
 	imports: [
@@ -25,4 +26,8 @@ import { AllExceptionsFilter } from "./filters/allExceptions.filter";
 		},
 	],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer): void {
+		consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+	}
+}
