64f41da9e4cc38c1e657f9d6f2591e79664053f2
Toby <40370496+BitOfAByte@users.noreply.github.com>
2023-06-23 08:48:56 +0200

    feat: added request logging (#65)
    * feat: added request logging
    
    * feat: changed filename
    
    ---------
    
    Co-authored-by: NullableDev <hopetoby@gmail.com>
    Co-authored-by: Ayush Chugh <ayushchugh2006@gmail.com>

---
+++ src/middleware/requestLogger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export default class RequestLoggerMiddleware implements NestMiddleware {
+    private logger: Logger = new Logger('HTTP')
+    use(req: Request, res: Response, done: NextFunction) {
+        const { ip, method, path: url } = req;
+		const userAgent = req.get("user-agent") || "";
+
+		res.on("close", () => {
+			const { statusCode } = res;
+			const contentLength = res.get("content-length");
+			this.logger.log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`);
+		});
+
+		done();
+    }
+}
